2bbfd0942bf771030ac7c6d27888c58a5727a4a7
Rafael Fernandes <rafael_mottarp@live.com>
2015-03-21 08:47:15 -0300

    Start DLL

---
+++ OWiSn32E/OWiSn32E/OWiSn32E.cpp
@@ -0,0 +1,10 @@
+// OWiSn32E.cpp : Defines the exported functions for the DLL application.
+//
+// Autor : GnomeXFire DLL Code DBZ Xenoverse 2015 - Beta Codigo Aberto
+// 
+#include "stdafx.h"
+#include "windows.h"
+#include "stdio.h"
+
+// Test Sig 
+
